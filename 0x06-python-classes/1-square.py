9d0e8274a66d444990c84acf190a6740f9108075
HASSANBOUDRRA <hassan.hbmama@gmail.com>
2023-06-27 16:47:23 -0700

    the function squre with __init___

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" define a class square."""
+
+
+class square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
