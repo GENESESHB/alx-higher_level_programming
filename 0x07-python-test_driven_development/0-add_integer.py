425d5521012cec11667b332bedf4385ac9fbe765
root <hassan.hbmama@gmail.com>
2023-07-02 13:57:45 +0000

    additonal two numbers and tests all senarios of code

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-
-
 """
-this the models for add_integer.py
+This is the module  0-add_integer.py
 """
 
 
 def add_integer(a, b=98):
     """
-    function that adds two integer
+    Function that adds 2 integers
     """
- if not isinstance(a, (int, float))
-        raise TypeError("a must be an integer or b must be an integer")
-    if not isinstance(b, (int, float))
-        raise TypeError("a must be an integer or b must be an integer")
-
-    a = int(a)
-    b = int(b)
-
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        raise TypeError("b must be an integer")
     return a + b
