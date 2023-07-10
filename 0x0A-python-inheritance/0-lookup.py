3a007581020c9d8997c13b3c3b4ea187a9f67bbd
root <hassan.hbmama@gmail.com>
2023-07-10 19:19:19 +0000

    the function can returns list of availables attributes of methode #lookup*/

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 
+""" this a function lookup """
+
+
 def lookup(obj):
+    """
+        this a function that returns the list of available attributes
+         and methods of an object.
+
+    args:
+        obj; the objets we search method for get attributes for.
+    Return:
+           list of attributes and methode.
+    """
     return dir(obj)
