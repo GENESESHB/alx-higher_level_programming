4ca2846bb8cd73e1fdaa9929d769ab8b126a80f7
HASSANBOUDRRA <hassan.hbmama@gmail.com>
2023-07-12 08:30:57 -0700

    hh

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
