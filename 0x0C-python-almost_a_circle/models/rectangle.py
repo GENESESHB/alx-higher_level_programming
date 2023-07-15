01faa429a58b719329019126027972b94a568bff
HASSANBOUDRRA <hassan.hbmama@gmail.com>
2023-07-15 13:36:06 -0700

    @properety and @....setter

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,3 +26,55 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+    @proporety
+    def width(self):
+        """Set/get the width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width (self, value):
+        if type (value) != int:
+            raise TypeError("value not a int")
+        if value <= 0:
+            raise ValueError("most be >= 0")
+        self.__width = width
+
+    @proporety
+    def height (self):
+        """Set/get the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height (self, value):
+        if type (value) != int:
+            raise TypeError("value not a int")
+        if value <= 0:
+            raise ValueError("most be >0")
+        self.__height = height
+
+    @proporety
+    def x(self):
+        """Set/get the x coordinate of the Rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type (value) != int:
+            raise TypeError("x most be integer")
+        if value < 0:
+            raise ValueError("x most be >= 0")
+        self.__x = x
+
+    @proporety
+    def y(self):
+        """set/get cordinator of triangle"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type (value) != int:
+            raise TypeError("y most be integer")
+        if value < 0:
+            raise ValueError("y most be >0")
+        self.__y = y
