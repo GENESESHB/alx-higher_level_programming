0e4d290e4a27cfcf3864fb6be1e1bfeea7852f10
hassan <hassan.hbmama@gmail.com>
2023-09-12 15:24:04 -0700

    add exp to RDM

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-module.exports = class square extends require('./4-rectangle.js') {
+module.exports = class Square extends require('./4-rectangle.js') {
   constructor (size) {
     super(size, size);
   }
