eb55d92e1d69b31c3ad210eee40a841a30e15bf5
hassan <hassan.hbmama@gmail.com>
2023-09-14 18:40:00 -0700

    the outpUt with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name starting with N
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` ORDER BY `id`")
+    states = cur.fetchall()
+    [print(state) for state in states if state[1][0] == "N"]
