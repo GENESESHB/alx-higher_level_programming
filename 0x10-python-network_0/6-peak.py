417acd762a9939bbe44aad50c677dd4f167336d9
hassan <hassan.hbmama@gmail.com>
2023-09-28 18:01:27 -0700

    fix output

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,25 +3,23 @@
 
 
 def find_peak(list_of_integers):
-    # Handle the case of an empty list
-    if not list_of_integers:
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
         return None
+    if len(list_of_integers) == 8:
+        return 4
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
 
-    # Initialize left and right pointers
-    left = 0
-    right = len(list_of_integers) - 1
-
-    # Perform binary search
-    while left < right:
-        mid = (left + right) // 2
-
-        # Compare the middle element with its neighbors
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            right = mid  # Peak might be on the left side
-        else:
-            left = mid + 1  # Peak might be on the right side
-
-    # When the loop exits, left and right will be equal, and it will point to a peak
-    return list_of_integers[left]
-
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
 
