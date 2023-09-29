c2903cc7bb40f07a98a6fc0bb46c45f932ebd40c
hassan <hassan.hbmama@gmail.com>
2023-09-28 19:53:54 -0700

    fatch_me

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script sends a POST request to 0.0.0.0:5000/catch_me causing the server to respond with "You got me!"
+curl -sLX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
+
