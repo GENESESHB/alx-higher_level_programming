44aff0734db89928f72d314c643fa86003da4969
hassan <hassan.hbmama@gmail.com>
2023-09-28 19:42:19 -0700

    binary tree search

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-"""Defines a peak-finding algorithm."""
-
+"""Defines algorithems peak."""
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
-    if list_of_integers == []:
+    """
+    Finds a peak in a list of unsorted integers using binary search.
+    """
+    if not list_of_integers:
         return None
 
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
+    left, right = 0, len(list_of_integers) - 1
 
-    mil = int(size / 2)
-    peak = list_of_integers[mil]
-    if peak > list_of_integers[mil - 1] and peak > list_of_integers[mil + 1]:
-        return peak
-    elif peak < list_of_integers[mil - 1]:
-        return find_peak(list_of_integers[:mil])
-    else:
-        return find_peak(list_of_integers[mil + 1:])
+    while left < right:
+        mid = (left + right) // 2
+
+        # Check if mid element is a peak
+        if list_of_integers[mid] > list_of_integers[mid - 1] and list_of_integers[mid] > list_of_integers[mid + 1]:
+            return list_of_integers[mid]
+        elif list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    # If there is no peak in the middle, return the remaining element
+    return list_of_integers[left]
+
