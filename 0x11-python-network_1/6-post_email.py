910f0143ee4e33d59231f007432315b6e042b93a
HASSAN-BOUDRAA <125823203+GENESESHB@users.noreply.github.com>
2023-10-19 04:27:10 +0200

    Delete 0x11-python-network_1 directory
    Signed-off-by: HASSAN-BOUDRAA <125823203+GENESESHB@users.noreply.github.com>

--- 0x11-python-network_1/6-post_email.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Send email using requests"""
-import requests
-import sys
-
-if __name__ == "__main__":
-    payload = {'email': sys.argv[2]}
-    r = requests.post(sys.argv[1], data=payload)
-    print(r.text)
