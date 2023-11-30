027493c9c21d92aa92b22c4243943fa134dd79d6
root <root@DESKTOP-MOC38TV>
2023-11-30 21:16:56 +0100

    add tasks 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in the url sends a POST request to the possed URL and dispaly the body of response
-curl -s "$1" -X POST -d "email=test@gmail.&subject=I will always be here for PLD"
+curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
