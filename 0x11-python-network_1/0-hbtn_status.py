4b5257b8cd85746ba11ac37b0b28729d39a0a9d4
root <root@DESKTOP-MOC38TV>
2023-12-01 20:09:15 +0100

    fix prbs output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import urllib.request
+"""a script that fetches https://alx-intranet.hbtn.io/status"""
+from urllib.request import urlopen
 
-
-url = 'https://intranet.hbtn.io/status'
-request = urllib.request.Request(url)
-request.add_header('User-Agent', 'Mozilla/5.0')
-
-try:
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print(f"HTTPError: {e}")
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urlopen(url) as response:
+        response_content = response.read()
+        response_type = type(response_content)
+        response_utf_content = response_content.decode('utf-8')
+    print("Body response:")
+    print(f"\t- type: {response_type}")
+    print(f"\t- content: {response_content}")
+    print(f"\t- utf8 content: {response_utf_content}")
